1270872d79b923f6e32861061f6a0134f5dcbf3f
anighosal <aasgfamily@gmail.com>
2025-04-07 13:29:00 +0600

    link added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const dbName = "onlineNursery";
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://online-nursery-website-client-plum.vercel.app/",
     credentials: true,
   })
 );
